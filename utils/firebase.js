efa4019c592371223f790298551f021f0873be88
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-21 17:08:45 +0100

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "../firebase.json" assert { type: "json" };
+import serviceAccount from "../firebase.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
